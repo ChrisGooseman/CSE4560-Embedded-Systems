4f30183bf10f039e479e28020ae13d94cf1c46f5
Chris Guzman <guzman.s.christopher@gmail.com>
2021-06-26 23:57:39 -0700

    Lab 2 - Task 2
    Task 2 for lab 2 completed.

---
+++ lab2/task_2.c
@@ -0,0 +1,34 @@
+/*
+ *
+ */
+
+#include "lab2.h"                               // Lab specific defines/declarations
+
+#define LED_RED         (0x01)
+#define RGB_RED         (0x02)
+#define RGB_GREEN       (0x04)
+#define RGB_BLUE        (0x08)
+
+void task_2(void)
+{
+  // Declare a delay counter
+  volatile uint16_t i;
+  //
+  // TODO: Declare a toggling counter
+  volatile uint32_t counter = 0;
+
+
+  while (1)
+  {
+    // Toggle an LED
+    toggle(LED_RED);
+
+    //
+    // TODO: Print and increment the counter
+    uart_println("Counter Value: %u", counter);
+    counter++;
+
+    // Delay of ~166ms
+    for (i = 0; i < 50000; i++);
+  }
+}
